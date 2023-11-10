5fbd530443802ccadc93e4d81aeee7b921c999a0
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-11-10 20:51:11 +0630

    hot Reload Problem Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first App'),
-      backgroundColor: const Color.fromARGB(255, 172, 125, 121),
-    ),
-    body: Center(
-      child:Text(
-        "my body",
-        style:TextStyle(
-            color: Colors.red[600],
-            fontFamily: 'IndieFlower',
-            fontSize: 60,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 12,
-          ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('click'),
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+ const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            title: Text('My first App'),
+            backgroundColor: const Color.fromARGB(255, 172, 125, 121),
+          ),
+          body: Center(
+            child:Text(
+              "my hah",
+              style:TextStyle(
+                  color: Colors.red[600],
+                  fontFamily: 'IndieFlower',
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 12,
+                ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){},
+            backgroundColor: Colors.red[600],
+            child: Text('click'),
+          ),
+        );
+  }
+}
