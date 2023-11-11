39b81a8ea60c4cdf4ad5b8a820358db1bf060331
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-11-11 20:11:17 +0630

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home:Home(),
 ));
 
 class Home extends StatelessWidget {
- const Home({Key? key}) : super(key: key);
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
-            title: Text('My Second App'),
-            backgroundColor: const Color.fromARGB(255, 172, 125, 121),
-          ),
-          body: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Image(
-                    image: NetworkImage('https://images.unsplash.com/photo-1547665979-bb809517610d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'),
-                    fit: BoxFit.cover,
-                  ),
+    return  Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("My ID Card !"),
+        centerTitle: true,
+        backgroundColor:Colors.grey[600],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage('https://thewellesleynews.com/wp-content/uploads/2020/09/avatar.jpg'),
+                  radius: 40,
                 ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            backgroundColor: Colors.red[600],
-            child: Text('click'),
-          ),
-        );
+              ),
+              Divider(
+                height: 60,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 5.0
+                )
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                  'Kaung Min Khant',
+                  style: TextStyle(
+                      color: Colors.amber,
+                      letterSpacing: 5.0,
+                      fontSize: 25
+                  )
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                  'Current Skill Level',
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 5.0
+                  )
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                  '10',
+                  style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  )
+              ),
+              SizedBox(height: 30.0),
+             Row(
+               children: [
+                 Icon(
+                    Icons.mail,
+                    color: Colors.grey,
+                 ),
+                 SizedBox(width: 10.0),
+                 Text(
+                     'kmk@gmail.com',
+                   style: TextStyle(
+                     color:Colors.grey[400],
+                     letterSpacing: 5,
+                   ),
+                 )
+               ],
+             )
+            ],
+        ),
+      ),
+    );
   }
 }
